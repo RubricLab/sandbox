f01dfc538d689f000e1011a3e09dbd6ae12f7646
rubrot <steeps.lackeys-0n@icloud.com>
2023-09-28 20:06:40 +0000

    Add cowsay page

---
+++ cowsay/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Cowsay() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>cowsay</h1>
+    </div>
+  );
+}
